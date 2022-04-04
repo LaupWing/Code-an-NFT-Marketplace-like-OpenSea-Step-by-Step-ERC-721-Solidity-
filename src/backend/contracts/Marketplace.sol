943d1677a5025f81c6257d7dd90b7d0a5237630a
LaupWing <loc_strict@hotmail.com>
2022-04-04 16:26:18 +0200

    Bougth event

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -26,6 +26,15 @@ contract Marketplace is ReentrancyGuard{
       address indexed seller
    );
 
+   event Bought (
+      uint itemId,
+      address indexed nft,
+      uint tokenId,
+      uint price,
+      address indexed seller,
+      address indexed buyer
+   );
+
    mapping(uint => Item) public items;
 
    constructor(uint _feePercent){
@@ -48,6 +57,40 @@ contract Marketplace is ReentrancyGuard{
          false
       );
 
+      emit Offered(
+         itemCount, 
+         address(_nft), 
+         _tokenId, 
+         _price, 
+         msg.sender
+      );
+   }
 
+   function purchaseItem(uint _itemId) external payable nonReentrant{
+      uint _totalPrice = getTotalPrice(_itemId);
+      Item storage item = items[_itemId];
+
+      require(_itemId > 0 && _itemId <= itemCount, 'Item doesnt exists');
+      require(msg.value >= _totalPrice, 'not enough ether to cover item price and market fee');
+      require(!item.sold, 'item already sold');
+
+      item.seller.transfer(item.price);
+      feeAccount.transfer(_totalPrice - item.price);
+
+      item.sold = true;
+      item.nft.transferFrom(address(this), msg.sender, item.tokenId);
+
+      emit Bought(
+         itemCount, 
+         address(item.nft), 
+         item.tokenId, 
+         item.price, 
+         item.seller, 
+         msg.sender
+      );
+   }
+
+   function getTotalPrice(uint _itemId) view public returns(uint){
+      return items[_itemId].price*(100 + feePercent)/100;
    }
 }
