d9f86725822d4dd2137528c3eadc557c491b981c
LaupWing <loc_strict@hotmail.com>
2022-04-04 15:14:55 +0200

    deploy marketplace

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -7,14 +7,17 @@ async function main() {
 
    // Get the ContractFactories and Signers here.
    const NFT = await ethers.getContractFactory("NFT");
-   // const Marketplace = await ethers.getContractFactory("Marketplace");
+   const Marketplace = await ethers.getContractFactory("Marketplace");
    // deploy contracts
-   // const marketplace = await Marketplace.deploy(1);
+   const marketplace = await Marketplace.deploy(1);
    const nft = await NFT.deploy();
    // Save copies of each contracts abi and address to the frontend.
    // saveFrontendFiles(marketplace, "Marketplace");
    console.log("NFT contract address", nft.address)
+   console.log("Marketplace contract address", marketplace.address)
+
    saveFrontendFiles(nft, "NFT");
+   saveFrontendFiles(marketplace, "Marketplace");
 }
 
 function saveFrontendFiles(contract, name) {
